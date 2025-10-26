63e360f24f6fb55383b8598d2ee787ae25dda4d5
Aleksandr Balashov <sashabalashov2019@gmail.com>
2025-10-26 21:42:44 +0500

    homework

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using NUnit.Framework.Legacy;
+using FluentAssertions;
 
 namespace HomeExercise.Tasks.ObjectComparison;
 public class ObjectComparison
@@ -15,15 +16,13 @@ public class ObjectComparison
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
         // Перепишите код на использование Fluent Assertions.
-        ClassicAssert.AreEqual(actualTsar.Name, expectedTsar.Name);
-        ClassicAssert.AreEqual(actualTsar.Age, expectedTsar.Age);
-        ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
-        ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-        ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+        
+        actualTsar.Should().BeEquivalentTo(expectedTsar, options => options
+            .Excluding(tsar => tsar.Id)
+            .Excluding(tsar => tsar.Parent!.Id));
+        // Мы заменили 8 строчек тестов одной, так же если параметры класса Person поменяются в будущем нам не потребуется писать доп тесты,
+        // потому что FluentAssertions сравнивает все свойства,
+        // через Excluding мы исключили параметр id, чтобы тест не падал
     }
 
     [Test]
@@ -34,7 +33,10 @@ public class ObjectComparison
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
-        // Какие недостатки у такого подхода? 
+        // Какие недостатки у такого подхода?
+        
+        // Нам придется переписывать метод AreEqual каждый раз когда меняется класс Person, при падении мы получим только
+        // Что ожидалось True, а пришло False или наоборт, непонятно где ошибка
         ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
     }
 
--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -7,25 +7,54 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void Test()
+    // тут решил добавить проверку на текст с ошибкой потому что иначе  если первое верно срабатывала вторая проверка падала и
+    // получалось неверно и непонятно что упало
+    // например precision 1 и scale 1 и тест первый ниже проходит хотя должен не проходить
+    [TestCase(-1, 2, "precision must be a positive number",
+        TestName = "precision не может быть отрицательным")]
+    [TestCase(0, 2, "precision must be a positive number",
+        TestName = "precision не может быть равен 0")]
+    [TestCase(1, 2, "precision must be a non-negative number less or equal than precision",
+        TestName = "scale не может быть больше precision")]
+    [TestCase(1, 1, "precision must be a non-negative number less or equal than precision",
+        TestName = "scale не может быть равен precision")]
+    [TestCase(1, -1, "precision must be a non-negative number less or equal than precision",
+        TestName = "scale не может быть отрицательным")]
+    public void ConstructorWhenParametersInvalid(int precision, int scale, string expectedMessage)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        Assert.That(
+            () => new NumberValidator(precision, scale, true),
+            Throws.TypeOf<ArgumentException>()
+                .With.Message.EqualTo(expectedMessage)
+        );
+    }
 
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+    [TestCase(1, 0, TestName = "scale может быть равен 0")]
+    [TestCase(7, 5, TestName = "общий тест")]
+    public void ConstructorWhenParametersValid(int precision, int scale)
+    {
+        Assert.DoesNotThrow(() => new NumberValidator(precision, scale, true));
+    }
+
+    
+    [TestCase(6, 2, false, "-1.23", true, TestName = "Валидное отрицательное число")]
+    [TestCase(6, 2, true, "+1.23", true, TestName = "Валидное положительное число")]
+    [TestCase(1, 0, true, "0", true, TestName = "Валидное целое число")]
+    [TestCase(4, 2, true, "+1.23", true, TestName = "Валидное число где знак и цифры укладываются в precision")]
+    [TestCase(6, 2, true, "+1,23", true, TestName = "Валидное число через запятую")]
+    [TestCase(3, 2, true, "00.00", false, TestName = "Ошибка: вышло за пределы precision")]
+    [TestCase(3, 2, true, "-0.00", false, TestName = "Ошибка: отрицательное число при onlyPositive=true")]
+    [TestCase(3, 2, true, "+0.00", false, TestName = "Ошибка: превышена точность из-за знака")]
+    [TestCase(6, 2, true, "0.000", false, TestName = "Ошибка: дробная часть превышает scale")]
+    [TestCase(3, 2, true, "-1.23", false, TestName = "Ошибка: отрицательное число при onlyPositive=true")]
+    [TestCase(3, 2, true, "a.sd", false, TestName = "Ошибка: нечисловая строка")]
+
+    public void IsValidNumber_Tests(int precision, int scale, bool onlyPositive, string input, bool expectedResult)
+        {
+            NumberValidator validator = new NumberValidator(precision, scale, onlyPositive);
+            Boolean result = validator.IsValidNumber(input);
+            
+            ClassicAssert.AreEqual(expectedResult, result,
+                $"Упал для значения '{input}' с параметрами precision = {precision}, scale = {scale}, onlyPositive = {onlyPositive}");
     }
 }
